fd7863ed346fe5b4906dc97585e123e24c42b450
Andriy <andy25771@gmail.com>
2017-02-02 09:02:25 +0000

    simplified solution, removed quicksort

--- js/index.js
+++ js/index.js
@@ -1,27 +1,14 @@
-function max(arry) {
-    var arr = quickSort(arry);
-    return arr.pop();
-    
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
-    var pivot = arr[Math.ceil(arr.length / 2)];
-    var smaller = [];
-    var bigger = [];
-    for (var i = 0, len = arr.length; i < len; i++) {
-        if (arr[i] > pivot) {
-            bigger.push(arr[i]);
-        } else if (arr[i] < pivot) {
-            smaller.push(arr[i]);
+let max = (arr) => {
+    let maxVal = 0;
+    arr.forEach((item) => {
+        if (item > maxVal) {
+            maxVal = item;
         }
-    }
-    var result = quickSort(smaller).concat(pivot, quickSort(bigger));
-    return result;
-}
-}
+    });
+    return maxVal;
+};
 
 
 
-var list = [3, 5, 8, 12, 4, 12, 35, 34, 45, 90, 1, 2, 6];
+let list = [3, 5, 8, 12, 4, 12, 35, 34, 45, 90, 1, 2, 6, 91];
 console.log(max(list));
