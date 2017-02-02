9a110ac091fb5bb66e06c6d5725616fe430a425b
Andriy <andy25771@gmail.com>
2017-02-02 09:59:18 +0000

    min and max with shift and pop

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let max = (arr) => {
-    let maxVal = 0;
+    let maxVal = arr.pop();
     arr.forEach((item) => {
         if (item > maxVal) {
             maxVal = item;
@@ -8,7 +8,16 @@ let max = (arr) => {
     return maxVal;
 };
 
-
+let min = (arr) => {
+    let minVal = arr.shift();
+    arr.forEach((item) => {
+        if (item < minVal) {
+            minVal = item;
+        }
+    });
+    return minVal;
+};
 
 let list = [3, 5, 8, 12, 4, 12, 35, 34, 45, 90, 1, 2, 6, 91];
 console.log(max(list));
+console.log(min(list));
